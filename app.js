93358eaf8d9cb73fcb8a62b81934f4ba11426a42
王泽平 <wangzeping1@cloudwalk.com>
2021-12-22 11:57:42 +0800

    实现session校验验证码

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const session = require('express-session');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 // 引入外部模块
@@ -15,6 +16,19 @@ app.use(express.static("static"))
 //配置第三方中间件获取post提交的数据
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+//配置session的中间件
+app.use(session({
+    secret: 'sessionkey', //服务器端生成 session 的签名
+    name:"wxyz", //修改session对应cookie的名称
+    resave: false, //强制保存 session 即使它并没有变化
+    saveUninitialized: true, //强制将未初始化的 session 存储
+    cookie: { // 设置cookie属性
+        maxAge: 1000*60*30,
+        secure: false  // true 表示只有https协议才能访问cookie  
+    },
+    rolling:true,  //在每次请求时强行设置 cookie，这将重置 cookie 过期时间（默认：false）
+}))
 
 //配置外部路由模块
 app.use("/admin",admin)
--- routers/admin/login.js
+++ routers/admin/login.js
@@ -3,17 +3,46 @@ const svgCaptcha = require('svg-captcha');
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.render("admin/login/login.html")
+    res.render("admin/login/login.html",{
+        "username":'',
+        "password":'',
+        "verifyTip":'',
+    })
 })
-router.get("/doLogin", (req, res) => {
-    res.send("执行登录")
+router.post("/doLogin", (req, res) => {
+
+    let username = req.body.username
+    let password = req.body.password
+    let verify = req.body.verify
+
+    // 1、判断验证码是否正确
+    // 2、判断用户名密码是否合法
+
+    if (verify.toLocaleLowerCase() != req.session.captcha.toLocaleLowerCase()) {
+        // res.render("admin/public/error.html", {
+        //     "redirectUrl": "/admin/login",
+        //     "message": "图形验证码输入错误"
+        // })
+        res.render("admin/login/login.html", {
+            "username": username,
+            "password": password,
+            "verifyTip": "图形验证码输入错误"
+        })
+        return
+    }
+
+    //验证成功
+    res.render("admin/public/success.html", {
+        "redirectUrl": "/admin",
+        "message": "登录成功"
+    })    
 })
 
  
 router.get('/verify', function (req, res) {
     var captcha = svgCaptcha.create();
-    // req.session.captcha = captcha.text;
-    console.log('验证码内容',captcha.text);
+    // 保存验证码到session中
+    req.session.captcha = captcha.text;
     res.type('svg');
     res.status(200).send(captcha.data);
 });
