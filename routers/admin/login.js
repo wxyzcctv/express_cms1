0beaa50646eae265a6ab6b9e48ffc2b6b3265a90
王泽平 <wangzeping1@cloudwalk.com>
2021-12-22 10:53:32 +0800

    实现静态界面和验证码生成

--- routers/admin/login.js
+++ routers/admin/login.js
@@ -1,11 +1,21 @@
 const express = require('express')
+const svgCaptcha = require('svg-captcha');
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.send("用户登录页面")
+    res.render("admin/login/login.html")
 })
 router.get("/doLogin", (req, res) => {
     res.send("执行登录")
 })
+
+ 
+router.get('/verify', function (req, res) {
+    var captcha = svgCaptcha.create();
+    // req.session.captcha = captcha.text;
+    console.log('验证码内容',captcha.text);
+    res.type('svg');
+    res.status(200).send(captcha.data);
+});
 
 module.exports = router
--- routers/admin/manager.js
+++ routers/admin/manager.js
@@ -3,25 +3,15 @@ const router = express.Router();
 const ManagerModel = require("../../model/managerModel");
 
 router.get('/', async (req,res)=>{
-    let result = await ManagerModel.find({});
-
-    console.log(result);
-
-    res.send('管理员界面')
+    res.render('admin/manager/index.html')
 })
 
 router.get('/add', (req,res)=>{
-    let manager = new ManagerModel({
-        username: '张三',
-        password: '12313445'
-    })
-    manager.save((err)=>{
-        if (err) {
-            return console.log(err);
-        }
-        console.log('添加管理员成功');
-    });
-    res.send('添加管理员')
+    res.render('admin/manager/add.html')
+})
+
+router.get('/edit', (req,res)=>{
+    res.render('admin/manager/edit.html')
 })
 
 module.exports = router;
