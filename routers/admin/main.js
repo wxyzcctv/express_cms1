6f6299821836edd4d4be03887624516faa3f8020
王泽平 <wangzeping1@cloudwalk.com>
2021-12-28 00:03:08 +0800

    使用ajax实现请求改变状态

--- routers/admin/main.js
+++ routers/admin/main.js
@@ -1,5 +1,14 @@
 const express = require('express')
 const router = express.Router();
+const FocusModel = require('../../model/focusModel');
+const ManagerModel = require("../../model/managerModel");
+const NavModel = require('../../model/navModel');
+
+const appModel = {
+    FocusModel,
+    ManagerModel,
+    NavModel
+}
 
 router.get('/', async (req,res)=>{
     res.render('admin/main/index.html')
@@ -7,6 +16,39 @@ router.get('/', async (req,res)=>{
 
 router.get('/welcome', (req,res)=>{
     res.send('欢迎来到后台管理中心')
+})
+
+router.get('/changeStatus', async (req,res)=>{
+    //1、获取要修改数据的id
+    //2、我们需要查询当前数据的状态 
+    //3、修改状态   0 修改成 1    1修改成0
+
+    // es6里面的属性名表达式
+    // var aaa="username"
+    // var obj={
+    //     [aaa]:"张三"
+    // }
+    // console.log(obj)
+
+
+    let id = req.query.id;
+    let model = req.query.model + "Model";
+    let field = req.query.field;
+    let obj = {};
+    let result = await appModel[model].find({"_id":id});
+    if (result.length > 0) {
+        result[0][field] === 1 ? obj = {[field]:0} : obj = {[field]:1};
+        await appModel[model].updateOne({"_id":id},obj);        
+        res.send({
+            success: true,
+            message: '修改状态成功'
+        });
+    }else{
+        res.send({
+            success: true,
+            message: '修改状态失败'
+        });
+    }
 })
 
 module.exports = router;
--- static/admin/js/base.js
+++ static/admin/js/base.js
@@ -1,5 +1,4 @@
 $(function () {
-
 	app.init();
 });
 
@@ -9,6 +8,7 @@ var app = {
 		this.toggleAside();
 		this.deleteConfirm();
 		this.resizeIframe();
+        this.changeStatus()
 	},
 	deleteConfirm() {
 		$('.delete').click(function () {
@@ -18,13 +18,33 @@ var app = {
 	},
 	resizeIframe() {
 		const heights = document.documentElement.clientHeight - 100;
-		document.getElementById('rightMain').height = heights;
+        let rightMainObj = document.getElementById('rightMain');
+        if (rightMainObj) {
+            rightMainObj.height = heights;            
+        }
 	},
 	toggleAside() {
 		$('.aside h4').click(function () {
 			$(this).siblings('ul').slideToggle();
 		})
-	}
+	},
+    changeStatus(){
+        $('.chStatus').click(function () {
+            let id = $(this).attr("data-id");
+            let model = $(this).attr('data-model');
+            let field = $(this).attr('data-field');
+            let el = $(this);
+            $.get('/admin/changeStatus',{id,model,field},function (response) {
+                if (response.success){
+					if(el.attr("src").indexOf("yes")!=-1){
+						el.attr("src", "/admin/images/no.gif");
+					}else{
+						el.attr("src", "/admin/images/yes.gif");
+					}
+				}
+            })
+        })
+    }
 };
 
 $(window).resize(function () {
