73760c4c75b53183af8d73a73026251d4f1d6681
王泽平 <wangzeping1@cloudwalk.com>
2022-01-08 22:12:35 +0800

    添加北京车展静态界面

--- routers/index.js
+++ routers/index.js
@@ -2,8 +2,23 @@ const express = require('express')
 
 const router = express.Router();
 
-router.get('/',(req,res)=>{
-    res.send('首页')
-});
+router.get("/",(req,res)=>{
+    res.render("default/index.html")
+ })
+ 
+ router.get("/overview.html",(req,res)=>{
+    res.render("default/overview.html")
+ })
+ 
+ router.get("/news.html",(req,res)=>{
+    res.render("default/news.html")
+ })
+ 
+ router.get("/services.html",(req,res)=>{
+    res.render("default/services.html")
+ })
+ router.get("/contact.html",(req,res)=>{
+    res.render("default/contact.html")
+ })
 
 module.exports = router;
